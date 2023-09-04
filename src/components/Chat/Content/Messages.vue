cf9f51c9dea0803b558b6eab09191307c3bfc58a
ilyaa123 <ilya.a2145@gmail.com>
2023-09-04 20:08:00 +0300

    add infinity scroll

--- src/components/Chat/Content/Messages.vue
+++ src/components/Chat/Content/Messages.vue
@@ -1,19 +1,37 @@
 <script setup lang="ts">
-// import { VInfiniteScroll } from 'vuetify/labs/VInfiniteScroll';
+import { VInfiniteScroll } from 'vuetify/labs/VInfiniteScroll';
 
 interface Props {
 	messages: string[];
 }
 
 defineProps<Props>();
+
+const handleOnLoad: VInfiniteScroll['onLoad'] = ({ done }) => {
+	setTimeout(() => {
+		done('ok');
+	}, 2000);
+};
 </script>
 <template>
 	<div class="messages__container">
-		<v-list lines="one">
-			<template v-for="message in messages" :key="message">
-				<v-list-item :title="message"></v-list-item>
+		<v-infinite-scroll
+			height="600"
+			width="100%"
+			side="start"
+			@load="handleOnLoad"
+		>
+			<template v-for="(message, index) in messages" :key="message">
+				<div
+					:class="[
+						'px-2',
+						index % 2 === 0 ? 'bg-grey-lighten-2' : ''
+					]"
+				>
+					<v-list-item :title="message"></v-list-item>
+				</div>
 			</template>
-		</v-list>
+		</v-infinite-scroll>
 	</div>
 </template>
 <style scoped>
